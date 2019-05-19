7474695ef3fd96b3d86bd416be5ce17c74c873af
Temi <karatemitayo@gmail.com>
2019-05-19 12:25:26 +0100

    created new component

---
+++ src/activatefixmoney/BodyActivateFixMoney.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BodyActivateFixMoney extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ marginTop: "10rem" }}>Hello From BodyActivateFixMoney</h1>
+      </div>
+    );
+  }
+}
